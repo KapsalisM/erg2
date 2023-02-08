32ef646604a2d5bd499395bdece26d7be3e75500
MICHAIL KAPSALIS MOSCHOVAKIS <93080653+KapsalisM@users.noreply.github.com>
2023-02-08 08:02:20 +0200

    Add files via upload

---
+++ Ergasia2/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace Ergasia2
+{
+    
+    public partial class Form1 : Form
+    {
+        OleDbConnection connection;
+        //string connectionstring = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Game_user.mdb";
+        String connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Game_user.mdb;";
+        public Form1(){ 
+            InitializeComponent();
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2= new Form2();
+            form2.Show();
+            this.Hide();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection =new OleDbConnection(connectionString);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            StringBuilder sb = new StringBuilder();
+            String query = "Select user from player where user ='"+ textBox1.Text +"';";
+            OleDbCommand cmd = connection.CreateCommand();
+            cmd.CommandText= query;
+            OleDbDataReader reader = cmd.ExecuteReader();
+            connection.Close();   
+        }
+    }
+
+}
---
+++ Ergasia2/Form2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ergasia2
+{
+    public partial class Form2 : Form
+    {
+        int score = 0;
+        int x;
+        int y;
+        Random rand = new Random();
+        //List<PictureBox> list = new List<PictureBox>();
+       
+        public Form2()
+        {
+
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //int x = rand.Next(0, this.Width - pictureBox1.Width);
+            //int y = rand.Next(0, this.Height - pictureBox1.Height);
+            //this.pictureBox1.Location = new System.Drawing.Point(x, y);
+            //score++;
+            //Console.WriteLine("score = "+score);
+            //Controls.Remove(pictureBox1);
+            MessageBox.Show("akis");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            x = rand.Next(0, this.Width - pictureBox1.Width);
+            y = rand.Next(0, this.Height - pictureBox1.Height);
+            this.pictureBox1.Location = new System.Drawing.Point(x, y);
+            score++;
+            Console.WriteLine("score = " + score);
+            if (score == 20) { }
+            /*Controls.Remove(pictureBox1);
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Image= Image.FromFile("C:\\programming\\C#\\Efarmogi2-master\\Ergasia2\\bin\\Debug\\cat.jpg");
+            pictureBox.Size = new System.Drawing.Size(90, 88);
+            int x = rand.Next(0, this.Width - pictureBox.Width);
+            int y = rand.Next(0, this.Height - pictureBox.Height);
+            pictureBox.Location = new Point(x, y);
+            Controls.Add(pictureBox);
+            */
+        }
+    }
+}
